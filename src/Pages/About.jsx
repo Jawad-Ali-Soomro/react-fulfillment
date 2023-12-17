5cdfd6a59151a0a49575a83827b137c93b60e092
suleman15 <sulemanahmed2704@gmail.com>
2023-12-16 18:53:02 -0800

    workingOnSoftwarePage

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import Header from '../Components/Desktop'
+import React from "react";
+import Header from "../Components/Desktop";
 
 const About = () => {
   return (
-    <div>
+    <div className="bg-black">
       <Header />
+      <div className="mt-[400px] bg-black">
+        <div>
+          <div>Why grow with Librex?</div>
+          <div>
+            Librex brings the capabilities of retail fulfillment to every
+            eCommerce seller
+          </div>
+          <a href="\">GO FOR IT</a>
+        </div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
